554ba94d710acc893ab4ba69c0fcff90906ac69e
Kevin <gaolushaobing@gmail.com>
2018-08-28 22:06:11 +0800

    dictionary done

--- learn01_06_dictionary.py
+++ learn01_06_dictionary.py
@@ -9,3 +9,34 @@ print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+# 删除键值对
+del alien_0['x_position']
+print(alien_0)
+
+del alien_0['y_position']
+print(alien_0)
+
+# 类似对象组成的字典
+favorite_languages = {
+    'kevin': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print("Kevin's favorite language is " +
+      favorite_languages['kevin'].title() + ".")
+
+# 遍历词典 
+for key, value in favorite_languages.items():
+    print("key: " + key + " value: " + value)
+
+# 遍历字典中的所有键 keys()
+for name in favorite_languages.keys():
+    print(name.title())
+print("--------------")
+
+# 遍历字典中所有的值values()
+for value in favorite_languages.values():
+    print(value.title())
