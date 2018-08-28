e03c9c8a8559978eef4b945b323540c07ec0ab35
Kevin <gaolushaobing@gmail.com>
2018-08-28 23:17:53 +0800

    add odd even

---
+++ learn01_07_user_input_03.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# int输入
+height = input("How tall are you, in inches?")
+height = int(height)
+
+if height >= 36:
+    print("OH! You are tall!")
+else:
+    print("OH! it is very short!")
---
+++ learn01_07_user_input_04.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# 求模运算符可以除了以后取余数
+# 判断一个数是奇数还是偶数
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\nThe number " + str(number) + " is odd.")
+
