ff2702c44d14c2c2ef3e5ab16898e93382fa36cf
Kevin <gaolushaobing@gmail.com>
2018-08-28 16:30:57 +0800

    list cut

--- learn01_04_operating_list.py
+++ learn01_04_operating_list.py
@@ -5,25 +5,19 @@ for magician in magicians:
     print(magician.title() + ", that was a great trick!")
     print("That is nice!")
 print("Thank you, everyone. That was a great magic show!")
-print("\n-------------\n")
 
 # 函数range()生成数字
 for value in range(1, 5):
     print(value)
-print("\n-------------\n")
 
 # list()可以转换列表
 numbers = list(range(1, 6))
 print(numbers)
 
-print("\n-------------\n")
-
 # range可以指定步长
 # 2开始 不断加2 达到或者超过11
 even_numbers = list(range(2, 11, 2))
 print(even_numbers)
-
-print("\n-------------\n")
 
 
 # 1~10的平方
@@ -45,3 +39,8 @@ squares = [value**2 for value in range(1, 11)]
 print(squares)
 
 print("\n-------------\n")
+
+# 列表切片
+letters = ['a', 'b', 'c', 'd', 'e', 'f']
+print(letters[0:3])
+print(letters[-3:])
