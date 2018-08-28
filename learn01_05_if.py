0cf978fc67531d8cec386987ba7b99494ebf1b1c
Kevin <gaolushaobing@gmail.com>
2018-08-28 17:18:47 +0800

    if car == 'bmw'

---
+++ learn01_05_if.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# 把bmw大写
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+        
+print("\n-------------\n")
+
+#不等于
