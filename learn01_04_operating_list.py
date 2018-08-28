ac251f643377b170ac3fa242d6c4af0f13702b87
Kevin <gaolushaobing@gmail.com>
2018-08-28 15:37:20 +0800

    add list for in

--- learn01_04_operating_list.py
+++ learn01_04_operating_list.py
@@ -1,7 +1,10 @@
-#-*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 # for 遍历
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
-  print(magician.title() + ", that was a great trick!")
-  print("That is nice!")
-print
+    print(magician.title() + ", that was a great trick!")
+    print("That is nice!")
+print("Thank you, everyone. That was a great magic show!")
+print("\n-------------\n")
+
+
