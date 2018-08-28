428bada0ef19175364c4893d5c1e6aa347d63e30
Kevin <gaolushaobing@gmail.com>
2018-08-28 21:46:28 +0800

    add alien_0['dd']

---
+++ learn01_06_dictionary.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+print(alien_0)
+
+# 为字典添加信息
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
