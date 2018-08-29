eb6425c3e46a1cc7c537928b114d78114a0db837
Kevin <gaolushaobing@gmail.com>
2018-08-29 17:55:26 +0800

    add args basic

--- learn02_08_method_01_basic.py
+++ learn02_08_method_01_basic.py
@@ -15,3 +15,15 @@ def describe_pet(animal_type, pet_name):
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 describe_pet('hamster', 'Jerry')
 describe_pet('cat', 'tom')
+
+# 严格对应的关键词实参, 顺序无所谓的
+describe_pet(animal_type='hamster', pet_name='jerry')
+describe_pet(pet_name='tom', animal_type='cat')
+
+print("--- ---")
+# 形参的默认值
+def describe_pet2(animal_type, pet_name='tom'):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+describe_pet2(animal_type='hamster', pet_name='jerry')
+describe_pet2('cat')
