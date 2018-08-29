e05dc5fdce2db13f142584b85879df20f253c6af
Kevin <gaolushaobing@gmail.com>
2018-08-29 18:02:58 +0800

    add formatted name

---
+++ learn02_08_method_02_return.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# 返回值，一个完整的名字
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
