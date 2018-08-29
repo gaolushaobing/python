6b5bf2e52df8c46ec9c10381e661216969624dc7
Kevin <gaolushaobing@gmail.com>
2018-08-29 16:48:40 +0800

    add remove item

--- learn02_07_while_06_del_item.py
+++ learn02_07_while_06_del_item.py
@@ -1,2 +1,8 @@
 # -*- coding:utf-8 -*-
 # 利用while和remove()来删除多余的重复元素
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
