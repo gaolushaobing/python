ce8458390f5ee20823f3183ffc83f9ccf3e5f175
Kevin <gaolushaobing@gmail.com>
2018-08-30 09:14:14 +0800

    add hitle

--- learn03_08_method_04_wihle_input.py
+++ learn03_08_method_04_wihle_input.py
@@ -6,6 +6,7 @@ def get_formatted_name(first_name, last_name):
     full_name = first_name + ' ' + last_name
     return full_name
 
+
 while True:
     print("\nPlease tell me your name: ")
     print("(enter 'q' at any time to quit)")
@@ -20,4 +21,3 @@ while True:
 
     formatted_name = get_formatted_name(f_name, l_name)
     print("\nHello, " + formatted_name + "!")
-    
