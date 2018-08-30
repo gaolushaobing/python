674a50c927171ab936b960579bb4d67a91792ec8
Kevin <gaolushaobing@gmail.com>
2018-08-30 08:32:59 +0800

    add return

---
+++ learn03_08_method_03_return_dic.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# 函数返回一个词典
+def build_person(first_name, last_name, age=''):
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
