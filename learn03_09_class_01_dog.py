8f34e6c59813bf8e5cca1496c56b4cb81dfb0d43
Kevin <gaolushaobing@gmail.com>
2018-08-30 16:54:41 +0800

    add class basic dog

--- learn03_09_class_01_dog.py
+++ learn03_09_class_01_dog.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# 创建类
 class Dog():
     def __init__(self, name, age):
         self.name = name
@@ -9,3 +10,14 @@ class Dog():
 
     def roll_over(self):
         print(self.name.title() + " rolled over!")
+
+# 创建实例
+tyke = Dog('tyke', '1')
+print("My dog's name is " + tyke.name.title() + ".")
+print("My dog is " + str(tyke.age) + " years old.")
+tyke.sit()
+tyke.roll_over()
+
+spike = Dog('spike', '7')
+spike.sit()
+spike.roll_over()
