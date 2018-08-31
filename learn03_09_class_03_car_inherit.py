5a0681f1b08edee0217e96c9b77a4aa3cb0cdddd
Kevin <gaolushaobing@gmail.com>
2018-08-31 08:10:10 +0800

    prent class

---
+++ learn03_09_class_03_car_inherit.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+# 创建一个父类汽车
+class Car():
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    
+    def get_descirptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+    
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+x = Car('audi', 'a8-w12', 2018)
+print(x.get_descirptive_name())
+x.increment_odometer(200)
+x.read_odometer()
